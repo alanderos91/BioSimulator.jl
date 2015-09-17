cac8d07e258aae9418995683bf31c6b929ee5a23
Alfonso Landeros <alfonsolanderos91@gmail.com>
2015-09-17 13:26:50 -0700

    Added core types and SSA and SAL algorithms.

--- src/BioSimulator.jl
+++ src/BioSimulator.jl
@@ -1,5 +1,20 @@
 module BioSimulator
 
-# package code goes here
+using Distributions
+using Gadfly
+
+export Species, Reaction, Network, Simulation, SimulationResult, PopulationTrace,
+  ssa_explicit, ssa_stepper!, sal_explicit, sal_stepper!, plot_trajectory
+
+include("species.jl")
+include("reaction.jl")
+include("network.jl")
+include("simulation.jl")
+
+include("tracing.jl")
+
+include("mass_action.jl")
+include("ssa.jl")
+include("sal.jl")
 
 end # module
