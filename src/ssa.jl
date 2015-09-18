afc6e1c0e818f653b1aab0fb65c76f0fc50092fc
Alfonso Landeros <alfonsolanderos91@gmail.com>
2015-09-18 14:46:21 -0700

    Updated to use 'SimulationResult'.

--- src/ssa.jl
+++ src/ssa.jl
@@ -19,49 +19,54 @@ function ssa_step!(spcs::Vector{Species}, rxns::Vector{Reaction}, a_total::Float
   return;
 end
 
-function ssa(model::Simulation, t_final::Float64; tracing::Bool=false, output::Symbol=:explicit, stepsize::Float64=0.0)
-
-  traces = Dict{ASCIIString, PopulationTrace}()
-  for s in model.initial
-    if s.istracked
-      traces[s.id] = PopulationTrace(s)
-    end
-  end
-  md = Dict()
+function ssa(model::Simulation, t_final::Float64; itr::Int=1, tracing::Bool=false, output::Symbol=:explicit, stepsize::Float64=0.0)
 
   #Unpack model
+  init = model.initial
   spcs = model.state
   rxns = model.rxns
   params = model.param
 
-  t = 0.0
+  md = Dict()
 
-  while t < t_final
-    a_total = 0.0
-    for r in rxns
-      propensity!(r, spcs, params)
-      a_total = a_total + r.propensity
+  # Results array
+  results = Dict{ASCIIString, PopulationTrace}[]
+  for i = 1:itr
+    copy!(spcs, init)
+    traces = init_traces(spcs)
+
+    t = 0.0
+
+    while t < t_final
+      a_total = 0.0
+      for r in rxns
+        propensity!(r, spcs, params)
+        a_total = a_total + r.propensity
+      end
+
+      τ = rand(Exponential(1/a_total))
+      if t + τ <= t_final
+        ssa_step!(spcs, rxns, a_total)
+      end
+      t = t + τ
+
+      update_traces!(traces, t, spcs, tracing)
     end
-
-    τ = rand(Exponential(1/a_total))
-    if t + τ <= t_final
-      ssa_step!(spcs, rxns, a_total)
-    end
-    t = t + τ
-
-    update_traces!(traces, t, spcs, tracing)
+    push!(results, traces)
   end
 
+  md["algorithm"] = "SSA"
+  md["iterations"] = itr
   md["duration"] = t_final
   if output == :explicit
     md["output"] = "Explicit"
   elseif output == :fixed && stepsize > 0.0
     md["output"] = "Fixed-Interval"
     md["stepsize"] = stepsize
-    traces = regularize(traces, stepsize, t_final)
+    results = regularize(results, stepsize, t_final)
   end
 
-  return SimulationResult("SSA", traces, md)
+  return SimulationResults(model.id, results, md)
 end
 
 function update!(spcs::Vector{Species}, r::Reaction)
