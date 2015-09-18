023cbc95cfc3a4886cf300eea43bf6a5b0683419
Alfonso Landeros <alfonsolanderos91@gmail.com>
2015-09-18 12:23:33 -0700

    Inner constructor protects against negative coefficients and initializes the 'propensity' field.

--- src/reaction.jl
+++ src/reaction.jl
@@ -1,9 +1,16 @@
 export Reaction
 
 type Reaction
-    id::ASCIIString
-    rate::ASCIIString
-    propensity::Float64
-    pre::Vector{Int}
-    post::Vector{Int}
+  id::ASCIIString
+  rate::ASCIIString
+  propensity::Float64
+  pre::Vector{Int}
+  post::Vector{Int}
+
+  function Reaction(id::ASCIIString, rate::ASCIIString, pre::Vector{Int}, post::Vector{Int})
+    if any(pre .< 0) || any(post .< 0)
+      error("Stoichiometric coefficients must be positive.")
+    end
+    return new(id, rate, 0.0, pre, post)
+  end
 end
