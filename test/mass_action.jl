6105b6e3fae2345c76c7707c2f07b3372ada3830
Alfonso Landeros <alfonsolanderos91@gmail.com>
2015-09-18 17:01:57 -0700

    Using new 'Reaction' constructor.

--- test/mass_action.jl
+++ test/mass_action.jl
@@ -9,19 +9,19 @@ push!(X_t, Species("Y", 500, true)) # 2
 push!(X_t, Species("XY",  1, true)) # 3
 
 # Zero-Order Reaction: 0 ---> X
-r[:zero] = Reaction("Immigration", "k0", 0.0, [0,0,0], [1,0,0])
+r[:zero] = Reaction("Immigration", "k0", [0,0,0], [1,0,0])
 param["k0"] = 0.1
 
 # First-Order Reaction: X ---> 0
-r[:first] = Reaction("Death", "k1", 0.0, [1,0,0],[0,0,0])
+r[:first] = Reaction("Death", "k1", [1,0,0],[0,0,0])
 param["k1"] = 0.01
 
 # Second-Order Reaction: X + Y ---> XY
-r[:second_a] = Reaction("Dimerization_XY", "k2a", 0.0, [1,1,0], [0,0,1])
+r[:second_a] = Reaction("Dimerization_XY", "k2a", [1,1,0], [0,0,1])
 param["k2a"] = 0.25
 
 # Second-Order Reaction: X + X ---> Y
-r[:second_b] = Reaction("Dimerization_Y", "k2b", 0.0, [2,0,0], [0,1,0])
+r[:second_b] = Reaction("Dimerization_Y", "k2b", [2,0,0], [0,1,0])
 param["k2b"] = 0.5
 
 @test BioSimulator.mass_action(r[:zero], X_t, param) == param["k0"]  # Propensity
