b2f90b827e751134af610ccdf58e96c8853877a7
Alfonso Landeros <alfonsolanderos91@gmail.com>
2015-09-18 18:56:05 -0700

    Refactored to adhere to style used for ssa.jl

--- src/sal.jl
+++ src/sal.jl
@@ -1,4 +1,4 @@
-export sal_explicit, sal_stepper!
+export sal
 
 function sal_step!(spcs::Vector{Species}, rxns::Vector{Reaction}, events::Vector{Int}, τ::Float64, α::Float64)
   while isbadleap(spcs, rxns, events)
@@ -9,140 +9,67 @@ function sal_step!(spcs::Vector{Species}, rxns::Vector{Reaction}, events::Vector
   return τ
 end
 
-function sal!(spcs::Vector{Species},
-              rxns::Vector{Reaction},
-              param::Dict{ASCIIString, Float64},
-              t_final::Float64,
-              ϵ::Float64,
-              δ::Float64,
-              α::Float64,
-              drdt::Vector{Float64},
-              events::Vector{Int})
-  t = 0.0
+function sal(model::Simulation, t_final::Float64;
+             itr::Int=1,
+             tracing::Bool=false,
+             output::Symbol=:explicit,
+             stepsize::Float64=0.0,
+             tol::Float64=0.125,
+             thrsh::Float64=100.0,
+             ctrct::Float64=0.75)
 
-  while t < t_final
-
-    a_total = 0.0
-    for r in rxns
-      r.propensity = mass_action(r, spcs, param)
-      a_total = a_total + r.propensity
-    end
-
-    τ = 0.0
-    if a_total < δ
-      u1 = rand()
-      u2 = rand()
-      τ = -log(u1) / a_total
-      jump = a_total * u2
-      ssa_step!(spcs, rxns, jump)
-    else
-      compute_time_derivatives!(drdt, spcs, rxns, param)
-      τ = tau_leap(rxns, param, drdt, ϵ)
-
-      generate_events!(events, rxns, drdt, τ)
-      τ = sal_step!(spcs, rxns, events, τ, α)
-    end
-
-    t = t + τ
-  end
-
-  return spcs
-end
-
-function sal_explicit(model::Simulation, t_final::Float64;
-                      tracing::Bool=false,
-                      tol::Float64=0.125,
-                      thrsh::Float64=100.0,
-                      ctrct::Float64=0.75)
-  traces = Dict{ASCIIString, PopulationTrace}()
-  for s in model.initial
-    if s.istracked
-      traces[s.id] = PopulationTrace(s)
-    end
-  end
-
-  # Unpack model
+  #Unpack model
+  init = model.initial
   spcs = model.state
   rxns = model.rxns
   params = model.param
+
+  md = Dict()
+
+  # Results array
+  results = Dict{ASCIIString, PopulationTrace}[]
 
   # Create additional arrays
   drdt = zeros(Float64, length(rxns))
   events = zeros(Int, length(rxns))
 
-  t = 0.0
-  while t < t_final
-    a_total = 0.0
-    for r in rxns
-      r.propensity = mass_action(r, spcs, params)
-      a_total = a_total + r.propensity
-    end
+  for i = 1:itr
+    reset!(spcs, init)
+    traces = init_traces(spcs)
 
-    τ = 0.0
-    if a_total < thrsh
-      u1 = rand()
-      u2 = rand()
-      τ = -log(u1) / a_total
-      if t + τ <= t_final
-        jump = a_total * u2
-        ssa_step!(spcs, rxns, jump)
+    t = 0.0
+    while t < t_final
+      a_total = 0.0
+      for r in rxns
+        r.propensity = mass_action(r, spcs, params)
+        a_total = a_total + r.propensity
       end
-    else
-      compute_time_derivatives!(drdt, spcs, rxns, params)
-      τ = tau_leap(rxns, params, drdt, tol)
 
-      if t + τ <= t_final
-        generate_events!(events, rxns, drdt, τ)
-        τ = sal_step!(spcs, rxns, events, τ, ctrct)
+      τ = 0.0
+      if a_total < thrsh
+        τ = rand(Exponential(1/a_total))
+        if t + τ <= t_final
+          ssa_step!(spcs, rxns, a_total)
+        end
+      else
+        compute_time_derivatives!(drdt, spcs, rxns, params)
+        τ = tau_leap(rxns, params, drdt, tol)
+
+        if t + τ <= t_final
+          generate_events!(events, rxns, drdt, τ)
+          τ = sal_step!(spcs, rxns, events, τ, ctrct)
+        end
       end
+
+      t = t + τ
+      update_traces!(traces, t, spcs, tracing)
     end
-
-    t = t + τ
-    update_traces!(traces, t, spcs, tracing)
+    push!(results, traces)
   end
 
-  result = SimulationResult("SAL (Fixed)", traces, Dict{Any,Any}())
-
-  result.metadata["duration"] = t_final
-
-  return result
-end
-
-function sal_stepper!(model::Simulation, steps::Int, stepsize::Float64;
-                      tracing::Bool=false,
-                      tol::Float64=0.125,
-                      thrsh::Float64=100.0,
-                      ctrct::Float64=0.75)
-  # TODO: How to initialize wrt to `tracing` var
-
-  traces = Dict{ASCIIString, PopulationTrace}()
-  for s in model.initial
-    if s.istracked
-      traces[s.id] = PopulationTrace(s)
-    end
-  end
-
-  # Unpack model
-  spcs = model.state
-  rxns = model.rxns
-  params = model.param
-
-  # Create additional arrays
-  drdt = zeros(Float64, length(rxns))
-  events = zeros(Int, length(rxns))
-
-  for i = 1:steps
-    sal!(spcs, rxns, params, stepsize, tol, thrsh, ctrct, drdt, events)
-
-    update_traces!(traces, i * stepsize, spcs, tracing)
-  end
-
-  result = SimulationResult("SAL (Fixed)", traces, Dict{Any,Any}())
-
-  result.metadata["duration"] = steps * stepsize
-  result.metadata["stepsize"] = stepsize
-
-  return result
+  set_metadata!(md, "SAL", t_final, itr, output, stepsize)
+  if output == :fixed results = regularize(results, stepsize, t_final) end
+  return SimulationResults(model.id, results, md)
 end
 
 function update!(spcs::Vector{Species}, r::Reaction, k::Int)
