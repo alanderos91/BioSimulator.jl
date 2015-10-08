60ce6ca9566e24f37612714e878beac402cb20ec
Alfonso Landeros <alfonsolanderos91@gmail.com>
2015-10-07 22:26:14 -0700

    Test for profiling NRM memory allocation.

---
+++ test/profile/profile_nrm.jl
@@ -0,0 +1,17 @@
+using BioSimulator
+
+x = [Species("Particle", 5, true)]
+r = [Reaction("Birth", "alpha", [1], [2]),
+     Reaction("Death", "mu", [1], [0]),
+     Reaction("Immigration", "nu", [0], [1])]
+p = Dict{ASCIIString, Float64}("alpha" => 2.0,
+                               "mu" => 1.0,
+                               "nu" => 0.5)
+kendall = Network("Kendall's Process", x, r, p);
+
+# Compile
+@time nrm(Simulation(kendall), 4.0)
+Profile.clear_malloc_data()
+
+# Test
+@time nrm(Simulation(kendall), 4.0)
