a70ce16f556d409d468d80a8574e5169e192cda2
Alfonso Landeros <alfonsolanderos91@gmail.com>
2015-10-09 16:33:23 -0700

    Handle passing special argumentsto SAL.

--- src/interface.jl
+++ src/interface.jl
@@ -28,7 +28,7 @@ function init_sr(::Uniform, spcs, n)
 	return SimulationResult(spcs, n)
 end
 
-function simulate(model::Simulation, t_final::Float64, with::Symbol=:ssa; o::OutputType=Uniform(), dt::Float64=1.0, itr::Int=1)
+function simulate(model::Simulation, t_final::Float64, with::Symbol=:ssa; o::OutputType=Uniform(), dt::Float64=1.0, itr::Int=1, args...)
 	if with == :ssa
 		ssa(model, t_final, o, dt, itr)
 	elseif with == :frm
@@ -36,6 +36,9 @@ function simulate(model::Simulation, t_final::Float64, with::Symbol=:ssa; o::Out
 	elseif with == :nrm
 		nrm(model, t_final, o, dt, itr)
 	elseif with == :sal
-		sal(model, t_final, o, dt, itr)
+		tol = haskey(args, :tol) ? args[:tol] : 0.125
+		thrsh = haskey(args, :thrsh) ? args[:thrsh] : 100.0
+		ctrct = haskey(args, :ctrct) ? args[:ctrct] : 0.75
+		sal(model, t_final, o, dt, itr, tol, thrsh, ctrct)
 	end
 end
