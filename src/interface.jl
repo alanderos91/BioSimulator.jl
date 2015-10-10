0d0e29df05a8a159c72617a701a5e5b09686b6a3
Alfonso Landeros <alfonsolanderos91@gmail.com>
2015-10-09 17:31:04 -0700

    Parse arguments correctly. Additional update! functions for filling up results arrays at the end of a simulation.

--- src/interface.jl
+++ src/interface.jl
@@ -3,16 +3,22 @@ export simulate, Explicit, Uniform
 abstract OutputType
 
 immutable Explicit <: OutputType end
-immutable Uniform <: OutputType end
 
 function update!(::Explicit, result, n, t, t_next, dt, j, spcs)
 	update!(result, t, spcs)
-	return t_next, j
+	return t, j
+end
+
+function update!(::Explicit, result, n, t_next, dt, j, spcs)
+	update!(result, t_next, spcs)
+	return;
 end
 
 function init_sr(::Explicit, spcs, n)
 	return SimulationResult(spcs)
 end
+
+immutable Uniform <: OutputType end
 
 function update!(::Uniform, result, n, t, t_next, dt, j, spcs)
 	while t >= t_next
@@ -24,11 +30,25 @@ function update!(::Uniform, result, n, t, t_next, dt, j, spcs)
 	return t_next, j
 end
 
+function update!(::Uniform, result, n, t_next, dt, j, spcs)
+	while j <= n
+		update!(result, t_next, spcs, j)
+		j = j + 1
+		t_next = t_next + 1
+	end
+	return;
+end
+
 function init_sr(::Uniform, spcs, n)
 	return SimulationResult(spcs, n)
 end
 
 function simulate(model::Simulation, t_final::Float64, with::Symbol=:ssa; o::OutputType=Uniform(), dt::Float64=1.0, itr::Int=1, args...)
+	d = Dict{Symbol, Float64}()
+	for (key, value) in args
+		d[key] = value
+	end
+
 	if with == :ssa
 		ssa(model, t_final, o, dt, itr)
 	elseif with == :frm
@@ -36,9 +56,9 @@ function simulate(model::Simulation, t_final::Float64, with::Symbol=:ssa; o::Out
 	elseif with == :nrm
 		nrm(model, t_final, o, dt, itr)
 	elseif with == :sal
-		tol = haskey(args, :tol) ? args[:tol] : 0.125
-		thrsh = haskey(args, :thrsh) ? args[:thrsh] : 100.0
-		ctrct = haskey(args, :ctrct) ? args[:ctrct] : 0.75
+		tol = haskey(d, :tol) ? d[:tol] : 0.125
+		thrsh = haskey(d, :thrsh) ? d[:thrsh] : 100.0
+		ctrct = haskey(d, :ctrct) ? d[:ctrct] : 0.75
 		sal(model, t_final, o, dt, itr, tol, thrsh, ctrct)
 	end
 end
