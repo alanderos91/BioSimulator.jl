e984f9d153d8a10928af148e435927370a4b45cc
Alfonso Landeros <alfonsolanderos91@gmail.com>
2015-10-09 17:56:29 -0700

    Revised to use new interface.

--- test/kendall.jl
+++ test/kendall.jl
@@ -15,14 +15,14 @@ function computed_mean(sj)
 end
 
 x = [Species("Particle", 5, true)]
+r = [Reaction("Birth", "alpha", [1], [2]),
+     Reaction("Death", "mu", [1], [0]),
+     Reaction("Immigration", "nu", [0], [1])]
+p = Dict{ASCIIString, Float64}("alpha" => 2.0,
+                               "mu" => 1.0,
+                               "nu" => 0.5)
 
-r1 = Reaction("Birth", "alpha", [1], [2])
-r2 = Reaction("Death", "mu", [1], [0])
-r3 = Reaction("Immigration", "nu", [0], [1])
-r = [r1, r2, r3]
-
-p = Dict{ASCIIString, Float64}("alpha" => 2.0, "mu" => 1.0, "nu" => 0.5)
-network = Network("Kendall's Process", x, r, p);
+kendall = Network("Kendall's Process", x, r, p);
 
 t_final = 4.0
 Δt = 0.1
@@ -31,53 +31,27 @@ seed = 5357
 # Compute mean for comparison
 points = round(Int, t_final/Δt) + 1
 t = linspace(0.0,t_final, points)
-m = kendall_mean(x[1].pop,t,p["alpha"],p["mu"],p["nu"])
+theoretical = kendall_mean(x[1].pop,t,p["alpha"],p["mu"],p["nu"])
+
+algorithms = [:ssa, :nrm, :sal, :frm]
 
 # Run SSA and SAL once to compile
 print("Precompiling..."); @time begin
-  ssa(Simulation(network), t_final)
-  dssa(Simulation(network), t_final)
-
-  frm(Simulation(network), t_final)
-
-  nrm(Simulation(network), t_final)
-  dnrm(Simulation(network), t_final)
-
-  sal(Simulation(network), t_final)
-  dsal(Simulation(network), t_final)
+  for a in algorithms
+    simulate(Simulation(kendall), t_final, a, o=Explicit(), itr=1)
+    simulate(Simulation(kendall), t_final, a, o=Uniform(), dt=0.1, itr=1)
+  end
 end
 
-print("[ SSA  ]")
-srand(seed)
-@time ssa1 = ssa(Simulation(network), t_final, itr=10^5)
-@test_approx_eq_eps computed_mean(ssa1) m[end] 1.0
+println("Running tests...")
+for a in algorithms
+  print(" * Explicit ", uppercase(string(a)))
+  srand(seed); @time result = simulate(Simulation(kendall), t_final, a, o=Explicit(), itr=10^5)
+  computed = computed_mean(result)
+  @test_approx_eq_eps computed theoretical[end] 1e0
 
-srand(seed)
-print("[ dSSA ]")
-@time ssa2 = dssa(Simulation(network), t_final, dt=t_final, itr=10^5)
-@test_approx_eq_eps computed_mean(ssa2) m[end] 1.0
-
-#print("[ FRM  ]")
-#srand(seed)
-#@time frm1 = frm(Simulation(network), t_final, itr=10^5)
-#@test_approx_eq_eps computed_mean(frm1) m[end] 1.0
-
-print("[ NRM  ]")
-srand(seed)
-@time nrm1 = nrm(Simulation(network), t_final, itr=10^5)
-@test_approx_eq_eps computed_mean(nrm1) m[end] 1.0
-
-print("[ dNRM ]")
-srand(seed)
-@time nrm2 = dnrm(Simulation(network), t_final, itr=10^5)
-@test_approx_eq_eps computed_mean(nrm2) m[end] 1.0
-
-srand(seed)
-print("[ SAL  ]")
-@time sal1 = sal(Simulation(network), t_final, itr=10^5)
-@test_approx_eq_eps computed_mean(sal1) m[end] 1.0
-
-srand(seed)
-print("[ dSAL ]")
-@time sal2 = dsal(Simulation(network), t_final, dt=t_final, itr=10^5)
-@test_approx_eq_eps computed_mean(sal2) m[end] 1.0
+  print(" *  Uniform ", uppercase(string(a)))
+  srand(seed); @time result = simulate(Simulation(kendall), t_final, a, o=Uniform(), dt=0.1, itr=10^5)
+  computed = computed_mean(result)
+  @test_approx_eq_eps computed theoretical[end] 1e1
+end
