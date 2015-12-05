2daa285d64170daecd099ae80f5b28b16793fc52
Alfonso Landeros <alfonsolanderos91@gmail.com>
2015-12-04 17:43:27 -0800

    Slight refactoring.

--- src/interface/species.jl
+++ src/interface/species.jl
@@ -1,15 +1,26 @@
 import Base.show
 
 type Species
-  id::ASCIIString
-  initial::Int
+  id::Symbol
+  population::Int
   istracked::Bool
+
+  function Species(id, value, istracked)
+    if value < 0
+      error("Species population must be nonnegative.")
+    end
+    new(id, value, istracked)
+  end
+end
+
+function Species(id, value::Int=0; istracked::Bool=true)
+  Species(id, value, istracked)
 end
 
 function Base.show(io::IO, x::Species)
   if x.istracked
-    @printf io "%6d; tracked" x.initial
+    @printf io "%6d; tracked"   x.population
   else
-    @printf io "%6d; untracked" x.initial
+    @printf io "%6d; untracked" x.population
   end
 end
