964e148e432ded56b3f4f36e0f30ea6afc052604
Alfonso Landeros <alfonsolanderos91@gmail.com>
2015-12-05 02:19:38 -0800

    Remove unused tests.

--- test/species.jl
+++
@@ -1,7 +0,0 @@
-# Setup
-init = [Species("X", 1, false)]
-curr = [Species("Y", 0, true)]
-# Copy init into curr, then change the population of curr[1]
-reset!(curr,init)
-curr[1].pop = 0
-@test init[1].pop != curr[1].pop
