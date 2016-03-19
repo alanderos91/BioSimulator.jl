ac917da8d1c6624cb19493ab47191f79e848f6f9
Alfonso Landeros <alfonsolanderos91@gmail.com>
2016-03-18 17:35:29 -0700

    Bigger default fonts. Added an interpolate function.

--- src/output/plot.jl
+++ src/output/plot.jl
@@ -1,6 +1,6 @@
 function plot_mean_timeseries(data, field, colnames...)
     df = getfield(data, field)
-    temp = flatten(df[:, [:Time; collect(colnames)]])
+    temp = flatten(df[:, [:time; collect(colnames)]])
     temp = aggregate(temp, [:time, :species], [mean, std])
     names!(temp, [:time, :species, :mean, :std])
     temp[:min] = max(0, temp[:mean] - temp[:std])
@@ -8,38 +8,37 @@ function plot_mean_timeseries(data, field, colnames...)
 
     plot(temp,
     x=:time, y=:mean, ymin=:min, ymax=:max, color=:species,
-    Geom.line, Geom.point, Geom.errorbar
+    Geom.line, Geom.point, Geom.errorbar, Theme(major_label_font_size=12pt,minor_label_font_size=12pt)
     )
 end
 
 function plot_sample_timeseries(data, field, nsamples, col)
     df = getfield(data, field)
-    temp = df[:, [:Time; col]]
-    npts  = length(unique(df[:Time]))
+    temp = df[:, [:time, col]]
+    npts  = length(unique(temp[:time]))
 
-    temp = flatten(df[1:nsamples * npts, :])
+    temp = flatten(temp[1:nsamples * npts, :])
     temp[:iteration] = repeat([string(i) for i in 1:nsamples], outer=[1], inner=[npts])
 
-    plot(temp, x=:time, y=:copy_number, ygroup=:species, color=:iteration,
-    Geom.subplot_grid(Geom.line, Geom.point))
+    plot(temp, x=:time, y=:copynumber, color=:iteration, Geom.line, Geom.point, Theme(major_label_font_size=12pt,minor_label_font_size=12pt))
 end
 
 function plot_histogram(data, field, t, colnames...)
     df = getfield(data, field)
-    temp = df[ df[:Time] .== t, :]
-    temp = flatten(temp[:, [:Time; collect(colnames)]])
-    plot(temp, x=:copy_number, color=:species, Geom.histogram)
+    temp = df[ df[:time] .== t, :]
+    temp = flatten(temp[:, [:time; collect(colnames)]])
+    plot(temp, x=:copynumber, color=:species, Geom.histogram, Theme(major_label_font_size=12pt,minor_label_font_size=12pt))
 end
 
 function flatten(df)
-    cols   = names(df)[2:end]
+    cols  = names(df)[2:end]
     ncols = length(cols)
-    npts  = length(unique(df[:Time]))
-    itr    = round(Int, length(df[:Time]) / npts)
+    npts  = length(unique(df[:time]))
+    itr   = round(Int, length(df[:time]) / npts)
 
     if ncols == 1
         temp = DataFrame(
-            time  = df[:Time],
+            time  = df[:time],
             copynumber = df[cols[1]],
             species = cols[1]
         )
@@ -51,11 +50,33 @@ function flatten(df)
         end
 
         temp = DataFrame(
-            time  = repeat(convert(Vector, df[:Time]), outer=[ncols]),
+            time  = repeat(convert(Vector, df[:time]), outer=[ncols]),
             copynumber = counts,
             species = repeat(cols, inner=[npts * itr])
         )
     end
 
+    return temp
+end
+
+function interpolate(df, final_t, dt, itr)
+    t = df[:time]
+    npts = round(Int, final_t / dt) + 1
+    inds = zeros(Int, itr*npts)
+    k = 1
+    next_t = 0.0
+    for i = 1:length(t)
+        if t[i] >= next_t
+            inds[k] = i
+            next_t = next_t + dt
+            k = k + 1
+        end
+
+        if next_t > final_t
+            next_t = 0.0
+        end
+    end
+    temp = df[inds, :]
+    temp[:time] = repeat(collect(linspace(0.0, final_t, npts)), outer=[itr])
     return temp
 end
