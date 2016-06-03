4fa8b1af5b2ba1e78006072662de26939c2cf907
Alfonso Landeros <alfonsolanderos91@gmail.com>
2016-06-02 18:52:54 -0700

    Doc string, use constructor

--- src/interface/parameter.jl
+++ src/interface/parameter.jl
@@ -2,26 +2,42 @@ import Base.show, Base.convert, Base.promote_rule
 import Base.+, Base.-, Base.*, Base./, Base.^, Base.<, Base.>, Base.<=, Base.>=, Base.==
 import Base.exp, Base.log, Base.abs, Base.-
 
-type Parameter
-  id::Symbol
-  value::Float64
+"""
+```
+Parameter(id, value; label)
+```
 
-  description::AbstractString
+Construct a `Parameter` with identifier `id` that behaves like `Float64(value)`. An optional `label` may be provided as a an annotation.
+
+### Arguments
+- `id`: An identifier.
+- `value`: A numeric value.
+- `label`: A human-readable annotation.
+"""
+type Parameter
+  id    :: Symbol
+  value :: Float64
+  label :: UTF8String
 end
+
+id(p::Parameter) = p.value
+value(p::Parameter) = p.value
+label(p::Parameter) = p.label
 
 typealias Parameters Dict{Symbol, Parameter}
 
-function parameter(id::Symbol, value::Float64; description="")
-  return Parameter(id, value, description)
+function Parameter(id::Symbol, value; label="")
+  return Parameter(id, Float64(value), label)
 end
 
 function Base.show(io::IO, p::Parameter)
-  @printf io " * %s= %f   (%s)" p.id p.value p.description
+  str = (label(p) == "") ? label(p) : string("; ", label(p))
+  println(io, id(p), "=", value(p), str)
 end
 
 Base.convert{T<:Number}(::Type{T}, p::Parameter) = convert(T, p.value)
 
-Base.promote_rule{T<:Number}(::Type{Parameter}, ::Type{T}) = promote_rule(Float64, T)
+Base.promote_rule{T<:Number}(::Type{Parameter}, ::Type{T}) = Float64
 
 for op in [:(Base.(:+)),
            :(Base.(:-)),
