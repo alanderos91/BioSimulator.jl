533aaae5b9cea50aae3b220b7e7d60e9640b9925
Alfonso Landeros <alfonsolanderos91@gmail.com>
2016-06-02 18:52:33 -0700

    Doc string; remove objects

--- src/interface/network.jl
+++ src/interface/network.jl
@@ -1,35 +1,76 @@
 import Base.show
 
+"""
+```
+Network(id)
+```
+
+Constructs an empty `Network` that models a system of interacting particles. A particle is represented by a `Species`; an interaction is represented by a `Reaction`. Model parameters (e.g. an intrinsic birth rate) are represented by a `Parameter` type.
+
+Adding a `Species`, `Reaction`, or `Parameter` is done using the `<=` operator:
+
+```
+m <= Species("X", 100) # Adds a Species
+m <= Reaction("birth", :α, :(X --> X + X)) # Adds a Reaction
+m <= Parameter(:α, 0.5) # Adds a Parameter
+```
+
+A fully specified `Network` model is simulated by calling `simulate`:
+
+```
+simulate(m, time=10.0, method=:SSA, output=:explicit)
+```
+
+A `Network` is visualized by calling `petrinet`:
+
+```
+petrinet(m)
+```
+
+See also: `Species`, `Reaction`, `Parameter`, `simulate`, `petrinet`
+
+### Arguments
+`id`: An string identifier for the `Network`.
+"""
 type Network
-  id::ASCIIString
-  species::Dict{Symbol,Species}
-  reactions::Dict{Symbol,Reaction}
-  parameters::Dict{Symbol,Parameter}
+  id :: UTF8String
+
+  species_list   :: Dict{Symbol,Species}
+  reaction_list  :: Dict{Symbol,Reaction}
+  parameter_list :: Dict{Symbol,Parameter}
 
   function Network(id)
     s = Dict{Symbol,Species}()
     r = Dict{Symbol,Reaction}()
     p = Dict{Symbol,Parameter}()
-    return new(id, s, r, p)
+    return new(UTF8String(id), s, r, p)
   end
 end
 
+species_list(x::Network)   = x.species_list
+reaction_list(x::Network)  = x.reaction_list
+parameter_list(x::Network) = x.parameter_list
+
+n_species(x::Network)    = length(species_list(x))
+n_reactions(x::Network)  = length(reaction_list(x))
+n_parameters(x::Network) = length(parameter_list(x))
+
 function Base.show(io::IO, x::Network)
-  print(io, "[ Model: $(x.id) ]\n")
-  print(io, " no. species:    $(length(x.species))\n")
-  print(io, " no. reactions:  $(length(x.reactions))\n")
-  print(io, " no. parameters: $(length(x.parameters))\n")
+  println(io, "[ Model: $(x.id) ]")
+  println(io, " no. species:    $(n_species(x))")
+  println(io, " no. reactions:  $(n_reactions(x))")
+  println(io, " no. parameters: $(n_parameters(x))")
 end
 
 function add_object!(model, object, fieldname)
   dict = getfield(model, fieldname)
   id   = object.id
-  setindex!(dict, object, id)
+  setindex!(dict, object, symbol(id))
   return dict
 end
 
 function (<=)(model::Network, object::Species)
-  add_object!(model, object, :species)
+  add_object!(model, object, :species_list)
 end
 
 function (<=)(model::Network, object::Reaction)
@@ -38,20 +79,20 @@ function (<=)(model::Network, object::Reaction)
   try
     validate(reactants, model)
     validate(products,  model)
-    add_object!(model, object, :reactions)
+    add_object!(model, object, :reaction_list)
   catch ex
     rethrow(ex)
   end
 end
 
 function (<=)(model::Network, object::Parameter)
-  add_object!(model, object, :parameters)
+  add_object!(model, object, :parameter_list)
 end
 
 function validate(participants, model)
-  species_dict = model.species
+  species_dict = species_list(model)
   for species in keys(participants)
-    if !haskey(species_dict, species)
+    if !haskey(species_dict, symbol(species))
       error("$(species) is not defined.")
     end
   end
@@ -67,13 +108,13 @@ function rmv_object!(model, key, fieldname)
   return model
 end
 
-function (>=)(model::Network, x::Pair{Symbol,Symbol})
-  fieldname = x.first
-  key = x.second
-
-  if fieldname == :species
-    # remove all species refs in reactions?
-  end
-
-  rmv_object!(model, key, fieldname)
-end
+# function (>=)(model::Network, x::Pair{Symbol,UTF8String})
+#   fieldname = x.first
+#   key = x.second
+#
+#   if fieldname == :species_list || :species
+#     # remove all species refs in reactions?
+#   end
+#
+#   rmv_object!(model, key, fieldname)
+# end
