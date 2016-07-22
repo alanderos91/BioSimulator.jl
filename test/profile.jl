5633736068de1c10fed8955688f40d1249f27e85
Alfonso Landeros <alfonsolanderos91@gmail.com>
2016-07-22 01:13:39 -0700

    better profiling tool

--- test/profile.jl
+++ test/profile.jl
@@ -1,14 +1,22 @@
 using BioSimulator
+
 include("test_models.jl")
 
-function test()
-    for alg in BioSimulator.ALGORITHMS
+function test(algorithms)
+
+    for alg in algorithms
         @time simulate(kendall(), with=alg)
     end
+
     Profile.clear_malloc_data()
-    for alg in BioSimulator.ALGORITHMS
+
+    for alg in algorithms
         @time simulate(kendall(), time=4.0, sampling_interval=0.5, method=alg, realizations=100_000)
     end
+
+    nothing
 end
 
-test()
+cmdargs = map(symbol, ARGS)
+
+test(intersect(BioSimulator.ALGORITHMS, cmdargs))
