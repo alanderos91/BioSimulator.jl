6611bcbad281f83a3b08cd7e945832c39e1b14c5
Alfonso Landeros <alfonsolanderos91@gmail.com>
2016-08-16 12:33:07 -0700

    begin revisions with this approach; wip please squash me

--- src/interface/simulate.jl
+++ src/interface/simulate.jl
@@ -1,87 +1,29 @@
-"""
-```
-simulate(m::Newtork; method=:SSA, time=1.0, output=:explicit, sampling_interval=1.0, realizations=1, kwargs...)
-```
+function simulate(algorithm, X0, r; time::Float64=1.0, sampling_interval::Float64=1.0, nrlz::Integer=1)
 
-### Arguments
-- `m`: The network object
+  Xt = similar(X0)
 
-### Optional Arguments
-- `method`: The algorithm used in the simulation.
-- `time`: The stopping time for the simulation.
-- `output`: The type of output to record. The `:full` option will record simulation history after every algorithm step, whereas `:partial` will record the history after according to `sampling_interval`.
-- `sampling_interval`: The time between updates when `output=:partial`. The default is to record after every unit of time, according to the scale implicit in a given model `m`.
-- `realizations`: The number of Monte Carlo realizations.
-- `kwargs`: Optional keyword arguments specific to a particular algorithm. Refer to the appropriate algorithm for details.
-"""
-function simulate(network::Network; method::Symbol=:SSA, time=1.0, output=:partial, sampling_interval=1.0, realizations=1, track=Symbol[], kwargs...)
+  npts = round(Int, time / sampling_interval + 1)
 
-  if method == :SSA
-    algorithm = ssa(time; kwargs...)
-  elseif method == :ODM
-    algorithm = odm(time; kwargs...)
-  elseif method == :FRM
-    algorithm = frm(time; kwargs...)
-  elseif method == :NRM
-    algorithm = nrm(time; kwargs...)
-  elseif method == :SAL
-    algorithm = sal(time; kwargs...)
-  else
-    error("$method is an unrecognized algorithm.")
-  end
+  output = PartialHistory(length(Xt), npts, nrlz, 0.0, time)
 
-  otype = (output == :full) ? Explicit() : Uniform()
-
-  # Create the main data structure used in the simulation
-  Xt, species_id, id2ind = make_species_arr(species_list(network))
-  rs = reaction_system(reaction_list(network), id2ind)
-  reaction_id = Symbol[]
-
-  m = Model(network.id, Xt, rs, parameter_list(network), deepcopy(Xt))
-
-  # Identify which objects to track
-  if isempty(track)
-      tracked_species = [ i for i in 1:length(species_id) ]
-      tracked_reactions = Int[]
-  else
-      tracked_species   = findin(species_id, track)
-      tracked_reactions = findin(reaction_id, track)
-  end
-
-  Xt_history = initialize_history(otype, Xt, id2ind, realizations, Int(time / sampling_interval) + 1, time)
-
-  simulate(m, algorithm, otype, realizations, Xt_history)
+  simulate!(output, Xt, algorithm, X0, r, nrlz)
 end
 
-function simulate(m::Model, algorithm::Algorithm, output::OutputType, realizations, Xt_history)
-  initialize!(algorithm, m)
+function simulate!(output, Xt, algorithm, X0, r, nrlz) # nrlz is encoded in PartialHistory; refactor
 
-  Xt = m.Xt
-  rs = m.rs
-  p  = m.parameters
-  X₀ = m.X₀
+  for i in 1:nrlz
+    init!(algorithm)
+    copy!(Xt, X0)
+    compute_propensities!(r, Xt)
+    interval = 1
 
-  for i = 1:realizations
-    reset!(m)
-    reset!(algorithm, m)
-    while time(algorithm) < end_time(algorithm)
-      #@code_warntype update!(Xt_history, i, time(algorithm), Xt)
-      update!(Xt_history, i, time(algorithm), Xt)   # Record current state
-      step!(algorithm, Xt, rs, p)
+    while !done(algorithm)
+      output, interval = update!(output, Xt, get_time(algorithm), interval, i)
+      step!(algorithm, Xt, r)
     end
-    compute_statistics!(algorithm, i)
-    update!(Xt_history, i, time(algorithm), Xt) # Record final state
+
+    output, interval = update!(output, Xt, get_time(algorithm), interval, i)
   end
 
-  compile_metadata!(Xt_history, algorithm, realizations, output)
-
-  return Xt_history
-end
-
-function initialize_history(::Explicit, Xt, id2ind, nrlz, nstates, t)
-  return SimData(Xt, nrlz, id2ind)
-end
-
-function initialize_history(::Uniform, Xt, id2ind, nrlz, nstates, t)
-  return SimData(Xt, nrlz, id2ind, nstates, t)
+  return output
 end
