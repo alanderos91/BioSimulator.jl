38ff6da05325513d5745c7ccb915427782945dbe
Alfonso Landeros <alfonsolanderos91@gmail.com>
2016-12-23 22:24:39 -0800

    Fix error propagation. Need to add SIR test that helped identify this issue.

--- src/backend/pvec.jl
+++ src/backend/pvec.jl
@@ -1,5 +1,3 @@
-const RELATIVE_ERROR = 2.0^(-32)
-
 type PropensityVector{T <: AbstractFloat} <: AbstractVector{T}
   cache       :: Vector{T}
   intensity   :: T
@@ -16,13 +14,26 @@ call{T}(::Type{PVec}, x::Vector{T})  = PVec{T}(x)
 
 ##### PVec interface #####
 intensity(x::PVec) = x.intensity
-isstable(x::PVec)  = (x.error_bound <= RELATIVE_ERROR * x.intensity)
+isstable{T}(x::PVec{T})  = (x.error_bound <= eps(T) * x.intensity)
 
 @fastmath function update_errorbound!{T}(x::PVec{T}, xi::T, i::Integer)
   x.error_bound = x.error_bound + eps(T) * (x.intensity + x[i] + xi)
 end
 
 @fastmath function update_intensity!{T}(x::PVec{T}, xi::T, i::Integer)
+  # if x.intensity - x[i] + xi < 0
+  #   error("""
+  #   Intensity update failed!
+  #   old a₀ = $(x.intensity)
+  #   new a₀ = $(x.intensity - x[i] + xi)
+  #
+  #   old x[$i] = $(x[i]) / $(typeof(x[i]))
+  #   new x[$i] = $(xi) / $(typeof(xi))
+  #
+  #   error_bound = $(x.error_bound)
+  #   is_stable?  = $(isstable(x))
+  #   """)
+  # end
   x.intensity = x.intensity - x[i] + xi
 end
 
