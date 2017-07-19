7b8644c8a906699d2910ea82e2ee646fc5c65871
Alfonso Landeros <alanderos@ucla.edu>
2017-07-19 15:22:24 -0600

    Remove outdated files

--- test/observer.jl
+++
@@ -1,7 +0,0 @@
-# Pre-allocated Observers should have is_preallocated set to true
-
-# Dynamically allocated Observers should have is_preallocated set to false
-
-# Check that all pre-allocated Observers are the same size
-
-# Check that all dynamically allocated Observers are the same size
