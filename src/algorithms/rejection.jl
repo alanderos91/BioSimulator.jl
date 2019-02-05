9a1f2c516be59ceb4455b684049e23fdec87dc17
Alfonso Landeros <alanderos@ucla.edu>
2019-02-05 03:30:45 -0800

    Keep working on this

--- src/algorithms/rejection.jl
+++ src/algorithms/rejection.jl
@@ -1,31 +1,34 @@
 mutable struct RejectionMethod{S<:RatesCacheTrait} <: DecoupledSearchAlgorithm
-
+  abstract_state::Vector{Tuple{Float64,Float64}}
+  abstract_rates::Vector{Tuple{Float64,Float64}}
 end
 
-RatesCache(::DirectMethod{S}) where S = S()
+RatesCache(::RejectionMethod{S}) where S = S()
 
 # TODO
 # develop structs for intervals
+# rewrite the search functions using the iteration interface
 
 # OUTLINE
 
 @inline function generate_jump(algorithm::RejectionMethod)
-  j, u = accept_reject(...)
+  j, u = accept_reject(RatesCache(algorithm), algorithm.abstract_rates, algorithm.total_rate)
   s = -log(u) / max(total_rate)
   
   return (j, s)
 end
 
-@inline function accept_reject()
+# rates should be an iterator over intervals_rates
+@inline function accept_reject(ctype::S, abstract_rates, total_rate) where S
   while rejected
     # search jumps based on upper bounds
-    j = search_jumps(...)
+    j = search_jumps(ctype, rates, upper_bound(abstract_rates))
 
-    rejected = rand() > min(rates[j]) / max(rates[j])
+    rejected = rand() > lower_bound(abstract_rates, j) / upper_bound(abstract_rates, j)
 
     if rejected
-      # evaluate propensity
-      rejected = rand() > rates[j] / max(rates[j])
+      # evaluate propensity; how to get state and model here?
+      rejected = rand() > rate_j / upper_bound(abstract_rates, j)
     end
 
     u *= rand()
