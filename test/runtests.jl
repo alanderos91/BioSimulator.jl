01ead5c959292f468844e6473fc693c01f911563
AlfonsoLanderos <alanderos@ucla.edu>
2019-06-23 23:44:36 -0700

    Enable all tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,14 +5,14 @@ for model in readdir("test-models")
   include(joinpath("test-models", model))
 end
 
-# include(joinpath("interface", "well-mixed", "network.jl"))
-#
-# include(joinpath("data-structures", "dep_graph.jl"))
-# include(joinpath("data-structures", "priority_queue.jl"))
-#
-# include(joinpath("model", "reaction_system.jl"))
-#
-# include(joinpath("simulators", "mean_convergence.jl"))
+include(joinpath("interface", "well-mixed", "network.jl"))
+
+include(joinpath("data-structures", "dep_graph.jl"))
+include(joinpath("data-structures", "priority_queue.jl"))
+
+include(joinpath("model", "reaction_system.jl"))
+
+include(joinpath("simulators", "mean_convergence.jl"))
 
 ##### spatial simulations
 include(joinpath("lattice.jl"))
