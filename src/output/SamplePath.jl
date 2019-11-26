ec5755448cd27e2aaa3eb790e806f6e1b161e50a
AlfonsoLanderos <alanderos@ucla.edu>
2019-11-26 15:46:07 -0800

    Allow selection of variables and make compatible with Ints

--- src/output/SamplePath.jl
+++ src/output/SamplePath.jl
@@ -64,7 +64,7 @@ end
 
 ##### interpolation
 function get_regular_path(xw::SamplePath, save_points)
-    yw = SamplePath(__extract(xw.u[1]), [save_points[1]])
+    yw = SamplePath([__extract(xw.u[1])], [save_points[1]])
 
     # copy data from the sample path
     for j in 2:length(xw.u)
@@ -79,33 +79,85 @@ function get_regular_ensemble(x::Ensemble, save_points)
 end
 
 ##### plotting recipes #####
+__make_indices(vars::Integer) = vars
+__make_indices(vars::NTuple{N,Integer}) where N = (N > 1 ? collect(vars) : vars[1])
+__make_indices(vars::AbstractVector{T}) where T <: Integer = vars
 
-# @recipe function f(xw::SamplePath{T}) where T <: AbstractVector
-#     seriestype --> :steppre
-#
-#     xw.t, xw'
-# end
-#
-# @recipe function f(xw::SamplePath{T,1}) where T <: AbstractVector
-#     seriestype --> :steppre
-#
-#     xw.t, xw.u
-# end
+@recipe function f(xw::SamplePath{T};
+    summary = :trajectory,
+    vars = nothing) where T <: Number
+
+    if summary == :trajectory
+        seriestype --> :steppre
+        n = length(xw[1])
+        if vars == nothing
+            xw.t, xw'
+        else
+            idxs = __make_indices(vars)
+
+            if idxs isa Integer
+                xw.t, xw[idxs,:]
+            else
+                xw.t, xw[idxs,:]'
+            end
+        end
+    elseif summary == :phase
+        idxs = (vars == nothing) ? (1,2) : (vars[1], vars[2])
+        xs = [xw[idxs[1],t] for t in eachindex(xw.t)]
+        ys = [xw[idxs[2],t] for t in eachindex(xw.t)]
+
+        legend --> nothing
+
+        @series begin
+            seriestype --> :scatter
+            marker_z --> xw.t
+
+            xs, ys
+        end
+
+        @series begin
+            seriestype --> :path
+            xs, ys
+        end
+    end
+end
 
 @recipe function f(ens::Ensemble;
     summary = :mean,
-    save_points = 0:ens[1].t[end],
+    save_points = nothing,
     error_style = :bars,
     timepoint = 1,
     vars = nothing)
 
-    reg = get_regular_ensemble(ens, save_points)
+    if save_points == nothing
+        m = length(ens[1].t)
+        if any(xw -> length(xw) != m, ens)
+            t_values = 0:ens[1].t[end]
+        else
+            t_values = ens[1].t
+        end
+    else
+
+    end
+
+    reg = get_regular_ensemble(ens, t_values)
 
     if summary == :mean
         # extract the series data
         ts = reg[1].t
-        xs = transpose(mean(reg))
-        xstd = transpose(std(reg))
+
+        tmp_xs = transpose(mean(reg))
+        tmp_xstd = transpose(std(reg))
+
+        if vars == nothing
+            xs = tmp_xs
+            xstd = tmp_xstd
+        else
+            idxs = __make_indices(vars)
+
+            xs = tmp_xs[:, idxs]
+            xstd = tmp_xstd[:, idxs]
+        end
 
         # make the default a scatter plot
         # and add bars to represent standard deviation
@@ -134,10 +186,19 @@ end
         xs = [tmp[idxs[1],t] for t in eachindex(reg[1].t)]
         ys = [tmp[idxs[2],t] for t in eachindex(reg[1].t)]
 
-        seriestype --> :scatter
-        marker_z --> reg[1].t
+        legend --> nothing
 
-        xs, ys
+        @series begin
+            seriestype --> :scatter
+            marker_z --> reg[1].t
+
+            xs, ys
+        end
+
+        @series begin
+            seriestype --> :path
+            xs, ys
+        end
     else
         error("Unknown option for summary_type: $(summary_type)")
     end
