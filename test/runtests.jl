92f72cacd4619e36a096b51ead21b59208bdf700
AlfonsoLanderos <alanderos@ucla.edu>
2020-01-30 20:35:19 -0800

    silly mistake

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,23 +1,23 @@
 using BioSimulator
 using Test, Statistics
 
-# for model in readdir("test-models")
-#   include(joinpath("test-models", model))
-# end
-#
-# include(joinpath("interface", "well-mixed", "network.jl"))
+for model in readdir("test-models")
+  include(joinpath("test-models", model))
+end
 
-# include(joinpath("data-structures", "dep_graph.jl"))
-# include(joinpath("data-structures", "priority_queue.jl"))
+include(joinpath("interface", "well-mixed", "network.jl"))
+
+include(joinpath("data-structures", "dep_graph.jl"))
+include(joinpath("data-structures", "priority_queue.jl"))
 
 include(joinpath("model", "reaction_system.jl"))
 
-# include(joinpath("simulators", "mean_convergence.jl"))
+include(joinpath("simulators", "mean_convergence.jl"))
 
-##### spatial simulations
-# include(joinpath("lattice.jl"))
-# include(joinpath("ips-interface.jl"))
-# include(joinpath("execute.jl"))
+#### spatial simulations
+include(joinpath("lattice.jl"))
+include(joinpath("ips-interface.jl"))
+include(joinpath("execute.jl"))
 
-##### simulation output
-# include(joinpath("output", "SamplePath.jl"))
+#### simulation output
+include(joinpath("output", "SamplePath.jl"))
